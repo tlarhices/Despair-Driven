86e0a50375dc5c75d8154c577cf30355d0ec73cf
Clerc Mathias <tlarhices@gmail.com>
2010-03-09 17:07:01 +0900

    idees pour la separation des quartiers

--- ville/ville.py
+++ ville/ville.py
@@ -127,24 +127,105 @@ class Quartier:
     for point in points:
       if points[point]!=2:
         OK=False
-        print "Quartier pas OK car",point,"==",points[point]
     return OK
     
   def rendOK(self, ville):
     if self.verifieOK():
+      print "Quartier OK"
       return
-    print "TODO : Quartier rendOK"
+      
+    print "Quartier pas OK"
+    mieux = True
+    while not self.verifieOK() and mieux:
+      mieux = self.light(ville)
+      if not self.verifieOK():
+        mieux = mieux or self.heavy(ville)
+      
+    self.affiche()
     
+  def light(self, ville):
+    mieux=False
+    points={}
+    for route in self.bords:
+      points[tuple(route.pointA)]=points.get(tuple(route.pointA), 0)+1
+      points[tuple(route.pointB)]=points.get(tuple(route.pointB), 0)+1
+      
+    pointsPasOK = []
+    for point in points:
+      if points[point]!=2:
+        pointsPasOK.append(point)
+        
+    for route in ville.routes:
+      if tuple(route.pointA) in pointsPasOK and tuple(route.pointB) in pointsPasOK:
+        if self.bords.count(route)>0:
+          pass
+        else:
+          self.bords.append(route)
+          mieux=True
+    if mieux:
+      print "light a servit a un truc !"
+    return mieux
+    
+    
+  def heavy(self, ville):
+    bary = self.barycentre()
+    contours = []
+    for point in ville.points:
+      print "test %i/%i\r" %(ville.points.index(point), len(ville.points)),
+      i = ville.intersectionne(bary, point)
+      if i==None:
+        contours.append(point)
+        
+    mieux=False
+    for route in ville.routes:
+      if (route.pointA in contours and route.pointB in contours):
+        if route not in self.bords:
+          self.bords.append(route)
+          mieux=True
+    print "Etait pas bien, nouveau status :",self.verifieOK(),"a fait un truc :", mieux
+    return mieux
+
+  racine=None
   def affiche(self):
+    if self.racine!=None:
+      self.supprime()
+    self.racine = NodePath("quartier")
+    self.racine.reparentTo(render)
     col = random.random(), random.random(), random.random()
     for route in self.bords:
       ligne = LineSegs()
       ligne.setColor(*col)
       ligne.setThickness(2.0);
-      ligne.moveTo(*route.pointA)
-      ligne.drawTo(*route.pointB)
-      mdl=render.attachNewNode(ligne.create())
+      a = route.pointA
+      a[2]=0.0
+      b = route.pointB
+      b[2]=0.0
+      ligne.moveTo(*a)
+      ligne.drawTo(*b)
+      mdl=self.racine.attachNewNode(ligne.create())
       mdl.setLightOff()
+      
+  def supprime(self):
+    if self.racine!=None:
+      self.racine.detachNode()
+      self.racine.removeNode()
+      self.racine=None
+      
+  def barycentre(self):
+    cx,cy,cz = 0.0,0.0,0.0
+    for route in self.bords:
+      px,py,pz = route.pointA
+      cx+=px
+      cy+=py
+      cz+=pz
+      px,py,pz = route.pointB
+      cx+=px
+      cy+=py
+      cz+=pz
+    cx=cx/(len(self.bords)*2)
+    cy=cy/(len(self.bords)*2)
+    cz=cz/(len(self.bords)*2)
+    return cx,cy,cz
 
 
 class Ville:
@@ -298,7 +379,6 @@ class Ville:
   def chercheQuartiers(self, route=None):
     if route == None:
       route = random.choice(self.routes)
-    print "teste", route
     A,B = route.getCoord()
     dx=B[0]-A[0]
     dy=B[1]-A[1]
@@ -360,12 +440,14 @@ class Ville:
         if (route.pointA in contours2 and route.pointB in contours2):
           q2.append(route)
           
-      q1=Quartier(q1, 1.0)
-      q2=Quartier(q2, 1.0)
-      q1.rendOK(self)
-      q2.rendOK(self)
-      q1.affiche()
-      q2.affiche()
+      if len(q1)>2:
+        q1=Quartier(q1, 1.0)
+        q1.rendOK(self)
+        q1.affiche()
+      if len(q2)>2:
+        q2=Quartier(q2, 1.0)
+        q2.rendOK(self)
+        q2.affiche()
     
   def charge(self, fichier):
     def getCoord(point):
